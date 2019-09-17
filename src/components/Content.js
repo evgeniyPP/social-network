5c1ab6e4f30ac2cc10e3b6fa43c78e93c99ca08a
evgeniyPP <amenat12@mail.ru>
2019-09-17 23:12:25 +0300

    structure created

---
+++ src/components/Content.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Content.css";
+
+const Content = () => {
+  return (
+    <div className="content">
+      <img src="https://picsum.photos/id/993/1000/150" alt="main" />
+      <p>Это Content</p>
+    </div>
+  );
+};
+
+export default Content;
